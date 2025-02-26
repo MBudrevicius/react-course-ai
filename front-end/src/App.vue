63e930b8ca654761407b4fb54827aeb578dc2f70
MBudrevicius <budreviciusmartynas@gmail.com>
2025-02-26 11:27:25 +0200

    Added a little presentation for first demo!

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,85 +1,57 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="container">
+    <h1>Greitai čia atsiras išsamūs React programavimo kursai!</h1>
+    <p class="subtext">Ko gali tikėtis ateityje:</p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <div class="screenshots">
+      <img v-for="(image, index) in images" :key="index" :src="image" :alt="'Svetainės dizainas ' + (index + 1)" />
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
+<script setup>
+import { ref } from 'vue';
+
+const images = ref([
+  './src/assets/prototype_images/image1.png',
+  './src/assets/prototype_images/image2.png',
+  './src/assets/prototype_images/image3.png',
+  './src/assets/prototype_images/image4.png'
+]);
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   text-align: center;
-  margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+h1 {
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 10px;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.subtext {
+  font-size: 1.2rem;
+  color: #666;
+  margin-bottom: 30px;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.screenshots {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
 }
 
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.screenshots img {
+  width: 100%;
+  max-width: 900px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
 }
 </style>
