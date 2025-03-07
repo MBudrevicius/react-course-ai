979722a886de8bc1fd3f7256f0641b4e853e4b0b
Pijus Grincevičius <pijus.grincevicius@ktu.edu>
2025-03-07 08:50:15 +0200

    Front-End: lessonAPI: Add function to retrieve lesson by ID with authentication

--- front-end/src/api/lessonAPI.js
+++ front-end/src/api/lessonAPI.js
@@ -21,3 +21,24 @@ export async function getLessonsTitles() {
         throw error;
     }
 }
+
+export async function getLessonById(id) {
+    try {
+        const token = getCookie('AuthToken');
+        const response = await axios.get(`http://localhost:5255/lessons/${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            withCredentials: true
+        });
+        return response.data;
+    } catch (error) {
+        if (error.response) {
+            console.log("Error status:", error.response.status);
+            console.log("Server response:", error.response.data);
+        } else {
+            console.log("Error:", error.message);
+        }
+        throw error;
+    }
+}
