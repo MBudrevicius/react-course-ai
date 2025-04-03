b079e77b268f751df1eaf49c91a0f116bff3854e
Jonas Laurinavičius <jonaslaurinavicius1@gmail.com>
2025-04-03 10:47:56 +0300

    added restriction to only .js files

--- front-end/src/components/UploadFile.vue
+++ front-end/src/components/UploadFile.vue
@@ -7,11 +7,11 @@ import { useRoute } from 'vue-router'
 
 const showScoreModal = ref(false);
 const modalMode = ref('');
-
 const fileContent = ref('');
 const fileInput = ref(null);
 const evaluationResult = ref(null);
 const hasSubmission = ref(false);
+const fileError = ref(''); 
 
 const route = useRoute();
 const lessonId = ref(route.params.id);
@@ -45,18 +45,25 @@ async function sendFile(){
     catch(error){
         console.error(error);
     }
-
 }
 
 function readSubmission(event) {
+    fileError.value = '';
     const file = event.target.files[0];
-    const reader = new FileReader();
 
+    if (file && !file.name.toLowerCase().endsWith('.js')) {
+        fileError.value = 'Only .js files are allowed.';
+        if(fileInput.value){
+            fileInput.value.value = null;
+        }
+        return;
+    }
+
+    const reader = new FileReader();
     reader.onload = () => {
         fileContent.value = reader.result;
         console.log("Extracted text:", fileContent.value);
     }
-
     reader.readAsText(file);
 }
 
@@ -82,7 +89,15 @@ async function clearInput(){
 <template>
     <label for="files">Įkelk failus čia:</label>
     <div>
-        <input type="file" id="files" name="files" @change="readSubmission" ref="fileInput"/>
+        <input 
+            type="file" 
+            id="files" 
+            name="files" 
+            accept=".js"
+            @change="readSubmission" 
+            ref="fileInput" 
+        />
+        <p v-if="fileError" style="color: red;">{{ fileError }}</p>
         <button type="submit" @click="toggleScoreModal('submit')">Pateikti</button>
         <button v-if="hasSubmission" @click="toggleScoreModal('best-solution')">
             Geriausias sprendimas
